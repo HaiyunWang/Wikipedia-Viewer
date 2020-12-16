837212f7ab544ff8c09105b5152557b1eb93e344
Wang Haiyun <haiyun@logeek.local>
2020-12-16 13:46:06 +1100

    add serach icon

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <h1 class="title">Wikipedia viewer</h1>
+    <form>
+      <input type="text" placeholder="Search..." name="search">
+    </form>
+    <Result/>
+  </div>  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Result from './components/Result.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    Result
   }
 }
 </script>
@@ -24,5 +26,22 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+input[type=text] {
+  width: 130px;
+  box-sizing: border-box;
+  border: 2px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+  background-color: white;
+  /*background-image: url('searchicon.png');*/
+  background-position: 10px 10px; 
+  background-repeat: no-repeat;
+  padding: 12px 20px 12px 40px;
+  -webkit-transition: width 0.4s ease-in-out;
+  transition: width 0.4s ease-in-out;
+}
+input[type=text]:focus {
+  width: 100%;
 }
 </style>
